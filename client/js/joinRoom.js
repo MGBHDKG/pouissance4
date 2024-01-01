f12db588a6cfac4a6a8871079aa675cd665fe945
Graggle <warren.buch16@gmail.com>
2024-01-02 00:04:18 +0100

    Front page d'accueil

---
+++ client/js/joinRoom.js
@@ -0,0 +1,29 @@
+const button = document.getElementById("submit-button");
+
+button.addEventListener("click", () => {
+    const roomName = document.getElementById("roomName").value;
+    const pseudo = document.getElementById("pseudo").value;
+
+    //A faire cote serveur
+
+    if(roomName === ""){
+        console.log("Veuillez entrer un nom de room !");
+        return;
+    }
+
+    if(roomName.length > 5){
+        console.log("Nom de room trop long !");
+        return;
+    }
+
+    if(pseudo === ""){
+        console.log("Veuillez entrer un pseudo !");
+        return;
+    }
+
+    if(pseudo.length > 5)
+    {
+        console.log("Pseudo trop long !");
+        return;
+    }
+});
