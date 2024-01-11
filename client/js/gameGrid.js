5394c5017120040045eb96b4f1a6b24d3e9ccc39
Graggle <warren.buch16@gmail.com>
2024-01-11 15:03:46 +0100

    Dessinage grille

--- client/js/gameGrid.js
+++ client/js/gameGrid.js
@@ -1,1 +1,16 @@
-console.log("helo")
+let canvas = document.querySelector("canvas");
+let context = canvas.getContext('2d');
+
+
+(function drawGrid(){
+    context.fillStyle = '#283618';
+    context.beginPath();
+
+    for(let i=0; i<7; i++){
+        for(let k=0; k<7; k++){
+            context.arc(40 + k*70, 40 + i*70, 30, 0, Math.PI * 2, false);
+            context.fill();
+            context.closePath();
+        }
+    }
+})()
