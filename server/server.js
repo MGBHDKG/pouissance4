62caa20539ea2a889f5b53af1cc03480af459267
Graggle <warren.buch16@gmail.com>
2024-01-30 23:37:11 +0100

    ...

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
