448164be0244a4fd029f556b3b9360f51931cf7e
Graggle <warren.buch16@gmail.com>
2024-01-31 11:46:09 +0100

    bah non en fait bref

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 
 const corsParams = {
-  origin: 'https://pouissance4.netlify.app',
+  origin: 'https://pouissance4.netlify.app/',
   methods: ['GET','POST'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
