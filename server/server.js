24d8b5a556f9c2a4f155baee8986fc71b39b2914
Graggle <warren.buch16@gmail.com>
2024-02-01 23:54:56 +0100

    Rollback server

--- server/server.js
+++ server/server.js
@@ -1,22 +1,12 @@
-import { Server } from 'socket.io';
-import express from "express";
-import cors from "cors";
+import { Server } from "socket.io";
 
 import insertCoin from "./my_modules/insertCoin.js";
 import secondPlayerJoin from "./my_modules/secondPlayerJoin.js";
 
-const corsParams = {
-  origin: '*',
-  methods: ['GET','POST'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin'],
-  exposedHeader: ['Authorization', 'Access-Control-Allow-Origin'],
-  credentials: true,
-};
-
 const io = new Server(3000, {
-  cors: {
-    origin: "*"
-  }
+  cors :{
+    origin: ["http://localhost:8080", "https://pouissance4.netlify.app"]
+  },
 });
 
 let rooms = new Map();
@@ -24,27 +14,25 @@ let grids = new Map();
 
 io.on("connection", (socket) => 
 {
-  // When a user joins a room
+  //When an user joins a room
   socket.on("joinRoom", (roomName, pseudo) => 
   {
     console.log(roomName, pseudo);
-
-    if (rooms.has(roomName)) 
-    {
+    
+    if(rooms.has(roomName)){
       secondPlayerJoin(roomName, pseudo, rooms, grids, socket, io);
       return;
     }
 
-    rooms.set(roomName, [{ player: pseudo, id: socket.id, isHisTurn: true, color: "red" }]);
+    rooms.set(roomName, [{player: pseudo, id: socket.id, isHisTurn: true, color: "red"}])
 
     socket.join(roomName);
 
     io.to(socket.id).emit("joinRoom", pseudo, null, roomName);
   });
 
-  // When a user injects a coin
-  socket.on("insertCoin", (roomName, col) => 
-  {
+  //When user injects a coin
+  socket.on("insertCoin", (roomName, col) => {
     insertCoin(roomName, col, grids, rooms, socket, io);
-  });
-});
+  })
+})
