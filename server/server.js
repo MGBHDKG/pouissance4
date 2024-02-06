8bccbf5e6a1ef969147595fe10e80df68d49c2b8
Graggle <warren.buch16@gmail.com>
2024-02-06 16:07:00 +0100

    aled

--- server/server.js
+++ server/server.js
@@ -5,7 +5,8 @@ import secondPlayerJoin from "./my_modules/secondPlayerJoin.js";
 
 const io = new Server(3000, {
   cors :{
-    origin: ["http://localhost:8080", "https://pouissance4.netlify.app"]
+    origin: ["http://localhost:8080", "https://pouissance4.netlify.app"],
+    allowedHeaders: ['Content-Type', 'Authorization']
   },
 });
 
